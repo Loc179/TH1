cf03881ba9600928449c6dd2c037fdb4c4f811f9
Nguyen Tat Loc <locnt179@gmail.com>
2023-09-14 06:14:19 +0700

    Add project files.

---
+++ TH1/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿namespace TH1.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; } // Mật khẩu
+        public Branch? Branch { get; set; } //Ngành học
+        public Gender? Gender { get; set; }// Giới tính
+        public bool IsRegular { get; set; } // Hệ: true-chính quy, false-phi cq
+        public string? Address { get; set; } // Địa chỉ
+        public DateTime DateOfBorth { get; set; } // Ngày sinh
+    }
+}
